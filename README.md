407956d04a66fb39a7312a2e19ba9cfe80918b35
niks255 <niks255292@gmail.com>
2019-02-17 17:55:12 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# exopen
+# exOpen
 A small executable that takes path from command line argument and opens it in windows explorer
+
+I wrote this to quickly open current directory from Total Commander button bar. That's it.
---
+++ exOpen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace exOpen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = args[0];
+                Process.Start(path);
+            } catch {
+            }
+        }
+    }
+}
